4bc3759a505e815f5b00e2b8091049248bca093d
Md.Anisur Rahman <tanimrahman78@gmail.com>
2023-10-24 09:10:46 +0600

    Update logistic_heart_.py

--- supervised/Regression/logistic_heart_.py
+++ supervised/Regression/logistic_heart_.py
@@ -47,10 +47,6 @@ Hungarian Institute of Cardiology. Budapest: Andras Janosi, M.D.
 University Hospital, Zurich, Switzerland: William Steinbrunn, M.D.
 University Hospital, Basel, Switzerland: Matthias Pfisterer, M.D.
 V.A. Medical Center, Long Beach and Cleveland Clinic Foundation: Robert Detrano, M.D., Ph.D.
-
-
-
-**TASK: Run the cell below to read in the data.**
 """
 
 df = pd.read_csv('/data/heart.csv')
