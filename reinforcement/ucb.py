61846c91fcc7066972e382f3e852f9d2261c6d56
tanim95 <tanimrahman78@gmail.com>
2023-12-02 19:26:25 +0600

    optimised ucb algorithm

--- reinforcement/ucb.py
+++ reinforcement/ucb.py
@@ -13,31 +13,37 @@ df = pd.read_csv('./data/Ads_CTR_Optimisation.csv')
 print(df.head())
 # implementing upper confidence bound(UCB) algorithm
 
-N = 10000
-d = 10
-add_selected = []
-number_of_selections = [0] * d
-sum_of_rewards = [0] * d
-total_reward = 0
-for i in range(0, N):
+
+N = 10000  # Number of rounds
+d = 10  # Number of ads
+
+add_selected = []  # List to store selected ads
+number_of_selections = np.zeros(d)  # Num of selections for each ad
+sum_of_rewards = np.zeros(d)  # Sum of rewards for each ad
+total_reward = 0  # Total reward obtained
+
+for n in range(1, N + 1):
     ad = 0
-    max_ucb = 0
-    for j in range(0, d):
-        if (number_of_selections[j] > 0):
-            avarage_reward = sum_of_rewards[j] / number_of_selections[j]
+    max_upper_bound = 0
+
+    for i in range(0, d):
+        if number_of_selections[i] > 0:
+            average_reward = sum_of_rewards[i] / number_of_selections[i]
             confidence_interval = math.sqrt(
-                3 / 2 * math.log(i+1) / number_of_selections[j])
-            upper_bound = avarage_reward + confidence_interval
+                2 * math.log(n) / number_of_selections[i])
+            upper_bound = average_reward + confidence_interval
         else:
-            upper_bound = 1e400
-        if (upper_bound > max_ucb):
-            max_ucb = upper_bound
-            ad = j
-            add_selected.append(ad)
-            number_of_selections[ad] += 1
-            reward = df.values[i, ad]
-            sum_of_rewards[ad] += reward
-            total_reward += reward
+            upper_bound = 1e400  # Large upper bound for unexplored arms
+
+        if upper_bound > max_upper_bound:
+            max_upper_bound = upper_bound
+            ad = i  # Updating the selected ad based on the highest UCB value
+
+    add_selected.append(ad)  # Record the selected ad
+    number_of_selections[ad] += 1  # Increment the count for the selected ad
+    reward = df.values[n - 1, ad]
+    sum_of_rewards[ad] += reward
+
 ads_df = pd.DataFrame({'Ad_Selected': add_selected})
 plt.figure(figsize=(8, 6), dpi=150)
 sns.countplot(data=ads_df, x='Ad_Selected', palette='viridis')
