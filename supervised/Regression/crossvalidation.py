c3615610cabcad977de02ad53f5ffacbfb46b96a
tanim95 <tanimrahman78@gmail.com>
2024-02-21 09:24:45 +0600

    .

--- supervised/Regression/crossvalidation.py
+++ supervised/Regression/crossvalidation.py
@@ -15,7 +15,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('/content/Advertising.csv')
+df = pd.read_csv('data\Advertising.csv')
 df.head()
 
 X = df.drop('sales', axis=1)
